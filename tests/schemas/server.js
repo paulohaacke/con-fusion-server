7566725949e2808bbba843c5645494cb2b14e6bf
Paulo Haacke <paulohaacke@gmail.com>
2017-01-29 21:12:34 -0200

    Node JS Course: Task 2 from assignment 2 done.

--- tests/schemas/server.js
+++ tests/schemas/server.js
@@ -1,26 +1,43 @@
 var genericServer = require('./generic-server');
 var url = 'mongodb://localhost:27017/conFusion';
 
-var dishes = require('../../models/dishes');
-genericServer.connectModel(url, dishes, genericServer.createStandardTest({
-    "name": "Uthapizza",
-    "image": "images/uthapizza.png",
-    "category": "mains",
-    "price": "4.99",
-    "description": "A unique . . .",
-    "comments": [{
-            "rating": 5,
-            "comment": "Imagine all the eatables, living in conFusion!",
-            "author": "John Lemon"
-        },
-        {
-            "rating": 4,
-            "comment": "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
-            "author": "Paul McVites"
+testModel = "Promotions";
+
+if (testModel === "Dish") {
+    var dishes = require('../../models/dishes');
+    genericServer.connectModel(url, dishes, genericServer.createStandardTest({
+        "name": "Uthapizza",
+        "image": "images/uthapizza.png",
+        "category": "mains",
+        "price": "4.99",
+        "description": "A unique . . .",
+        "comments": [{
+                "rating": 5,
+                "comment": "Imagine all the eatables, living in conFusion!",
+                "author": "John Lemon"
+            },
+            {
+                "rating": 4,
+                "comment": "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+                "author": "Paul McVites"
+            }
+        ]
+    }, {
+        $set: {
+            description: 'Updated Test'
         }
-    ]
-}, {
-    $set: {
-        description: 'Updated Test'
-    }
-}));
+    }));
+} else if (testModel === "Promotions") {
+    var promotions = require('../../models/promotions');
+    genericServer.connectModel(url, promotions, genericServer.createStandardTest({
+        "name": "Weekend Grand Buffet",
+        "image": "images/buffet.png",
+        "label": "New",
+        "price": "19.99",
+        "description": "Featuring . . ."
+    }, {
+        $set: {
+            description: 'Updated Test'
+        }
+    }));
+};
